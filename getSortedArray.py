96dfeaf6f0f033410720fe5dea16fc0c7764491e
Yevhenii Ganusich <33880254+y3vhenii@users.noreply.github.com>
2020-03-04 11:44:48 -0500

    Update getSortedArray.py

--- getSortedArray.py
+++ getSortedArray.py
@@ -5,7 +5,6 @@ def getSortedArray(n):
     tempVal = n
     sortedArray = []
     for i in range(n):
-        # Comment out print statement when
         sortedArray.append(tempVal)
         tempVal -= 1
     return sortedArray
